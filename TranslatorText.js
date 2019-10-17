7261280bfef47e761397415c1a9c952ae19d10ee
Malik2903 <56668874+Malik2903@users.noreply.github.com>
2019-10-17 11:49:45 +0530

    MVC

---
+++ TranslatorText.js
@@ -0,0 +1,33 @@
+﻿function TransalteText() {
+    $(document)
+    var languageCode = "&to=zh-Hans";
+            var enterText = $("#enterText").val();
+            if (1 <= $("#enterText").val().trim().length && languageCode != "NA") {
+                $('#enterText').removeClass('redBorder');
+                var url = '/TranslatorText/Translate';
+                var dataToSend = { "Text": enterText };
+                dataType: "json",
+                    $.ajax({
+                        url: url,
+                        data: dataToSend,
+                        type: 'POST',
+                        success: function (response) {
+                            var result = JSON.parse(response);
+                            var translatedText_it = result[0].translations[0].text;
+                            var translatedText_de = result[0].translations[1].text;
+                            var translatedText_es = result[0].translations[2].text;
+                            var translatedText_chinese = result[0].translations[3].text;
+                            var translatedText_ar = result[0].translations[4].text;
+                            $('#translatedText_it').val(translatedText_it);
+                            $('#translatedText_de').val(translatedText_de);
+                            $('#translatedText_es').val(translatedText_es);
+                            $('#translatedText_chinese').val(translatedText_chinese);
+                            $('#translatedText_ar').val(translatedText_ar);
+                        }
+                    })
+            }
+            else {
+                $('#enterText').addClass('redBorder');
+                $('#translatedText').val("");
+            }
+}
---
+++ TranslatorTextController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace ContentMGR.Controllers
+{
+    public class TranslatorTextController : Controller
+    {
+        // GET: TranslatorText
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<string> Translate(string text)
+        {
+            string key = "f18454e6276547169a81daac71ab706f";
+            string uri = "https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=it&to=de&to=es&to=zh-Hans&to=ar";
+            System.Object[] body = new System.Object[] { new { Text = text } };
+            var requestBody = JsonConvert.SerializeObject(body);
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
+            {
+                request.Method = HttpMethod.Post;
+                request.RequestUri = new Uri(uri);
+                request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+                request.Headers.Add("Ocp-Apim-Subscription-Key", key);
+                var response = await client.SendAsync(request);
+                var responseBody = await response.Content.ReadAsStringAsync();
+                dynamic result = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(responseBody), Formatting.Indented);
+                //var result = requestBody;
+                return result;
+            }
+        }
+    }
+}
